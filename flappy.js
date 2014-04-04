6772fdf7b0a3f2b580c5528541db0f8ae268d5b8
Karl Jakob Larsson <k.jakob.larsson@gmail.com>
2014-04-04 14:47:19 +0200

    Can draw Obstacles.

--- flappy.js
+++ flappy.js
@@ -1,3 +1,6 @@
+
+// All units in SI (seconds, pixels, meters etc) unless otherwise noted.
+
 var canvas = document.getElementById("a");
 canvas.width *= window.devicePixelRatio; // high-dpi adjustment
 canvas.height *= window.devicePixelRatio;
@@ -9,10 +12,8 @@ var rand = function(min, max) {
 var w = canvas.width;
 var h = canvas.height;
 
-c.fillStyle = "#FF00EE";
-
-var x = w/2;
-var y = h/2;
+// Constants
+var obstacleHoleSize = 70;
 
 var state = {
     gameover: false,
@@ -20,32 +21,113 @@ var state = {
         x: w/2,
         y: h/2,
     },
+    obstacles: {
+      a: 1,
+        halfWidth: 10,
+    },
 };
 
-function draw() {
-    c.clearRect(0,0,w,h);
-    c.fillRect(state.pos.x, state.pos.y, 20, 20);
-    //x += rand(-10,10);
-    //y += rand(-10,10);
+var prevTime = 0;
+
+
+function draw(t) {
+    if (state.gameover) {
+      drawGameover();
+    } else {
+      c.clearRect(0,0,w,h);
+      c.fillStyle = "#FF00EE";
+      c.fillRect(state.pos.x -10, state.pos.y -10, 20, 20);
+      
+      // debug
+      obs = getObstacleCorners(200, 0.3, 200);
+      debugger;
+      drawObstacle(obs);
+      debugger;
+      
+      c.font = "12pt Arial";
+      c.fillStyle = "#000";
+      c.fillText(t - state.startTime, 10, 10);
+    }
+}
+
+function drawObstacle(obstacle) {
+  c.fillStyle = "#060";
+  var top = obstacle.top;
+  var bot = obstacle.bottom;
+  
+  c.fillRect(top[0], top[1], state.obstacles.halfWidth, top[3]); // top
+  c.fillRect(bot[0], bot[1], state.obstacles.halfWidth, bot[3]); // bottom
+}
+
+function getObstacleCorners(x, holePos, holeSize) {
+  var halfWidth = state.obstacles.halfWidth;
+  
+  return {
+    top: [x - halfWidth, 0, x + halfWidth, holePos*h - holeSize/2],
+    bottom: [x - halfWidth, holePos*h + holeSize/2, x + halfWidth, h],
+  };
+}
+
+function newGame() {
+  c.clearRect(0,0,w,h);
+  c.fillStyle = "#FF00EE";
+  state.gameover = false;
+  state.pos = {
+        x: w/2,
+        y: h/2,
+    },
+  //state.obstacles = {};
+  state.startTime = null;
+}
+
+function update(t, dt) {
+  
+  if (key.up()) state.pos.y--;
+  if (key.left()) state.pos.x--;
+  if (key.down()) state.pos.y++;
+  if (key.right()) state.pos.x++;
+  
+  if ( (state.gameover) && key.space()) {
+    newGame();
+  }
+  
+  if (outOfBounds(state.pos)) {
+    state.gameover = true;
+  }
+}
+
+function drawGameover() {
+  c.clearRect(0,0,w,h);
+  c.fillStyle = "#333";
+  c.fillRect(0, 0, w, h);
+  
+  c.font = "48pt Arial";
+  c.fillStyle = "#F11";
+  c.fillText("Game Over", w/4, h/2);
+  
+  c.font = "24pt Times";
+  c.fillStyle = "#FF1";
+  c.fillText("Press space for new game.", w/4, 2*h/3);
+}
+
+function outOfBounds(p) {
+  return ((p.x < 0) ||  (p.x > w) || (p.y < 0) || (p.y > h));
+}
+
+function render(time) {
+    if (state.startTime === null) state.startTime = time;
+    var deltaTime = time - prevTime;
+    prevTime = time;
     
+    update(time, deltaTime);
+    draw(time);
+    animId = requestAnimationFrame(render);
 }
 
-function update() {
-  if (state.key.up()) y--;
-  if (state.key.left()) x--;
-  if (state.key.down()) y++;
-  if (state.key.right()) x++;
-}
+newGame();
+var animId = requestAnimationFrame(render);
 
-function render() {
-    draw();
-    update()
-    requestAnimationFrame(render);
-}
-requestAnimationFrame(render);
-
-
-state.key = {
+key = {
   _pressed: {},
 
   LEFT: 37,
@@ -65,11 +147,12 @@ state.key = {
     delete this._pressed[event.keyCode];
   },
   
-  left: function() {return this.isDown(state.key.LEFT)},
-  up: function() {return this.isDown(state.key.UP)},
-  right: function() {return this.isDown(state.key.RIGHT)},
-  down: function() {return this.isDown(state.key.DOWN)},
+  left: function() {return key.isDown(key.LEFT)},
+  up: function() {return key.isDown(key.UP)},
+  right: function() {return key.isDown(key.RIGHT)},
+  down: function() {return key.isDown(key.DOWN)},
+  space: function() {return key.isDown(32)},
 };
 
-window.addEventListener('keyup', function(event) { state.key.onKeyup(event); }, false);
-window.addEventListener('keydown', function(event) { state.key.onKeydown(event); }, false);
+window.addEventListener('keyup', function(event) { key.onKeyup(event); }, false);
+window.addEventListener('keydown', function(event) { key.onKeydown(event); }, false);
